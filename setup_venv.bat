83e0fbd3a6d4d69bce37a679893f3a867a8a1566
AlteonRea <alteonreastra@gmail.com>
2024-08-07 19:23:26 +0700

    add ignore

---
+++ setup_venv.bat
@@ -0,0 +1,10 @@
+cd backend/solver
+python -m venv susunbox_venv
+call susunbox_venv\Scripts\activate.bat
+
+pip install numpyencoder
+pip install matplotlib
+pip install numba
+pip install fpdf2
+
+echo "Setup done!"
